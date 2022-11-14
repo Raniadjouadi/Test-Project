d3ab2999844f3c422946a93143839917ff1851ac
Rania Djouadi <rania.djouadi2000@gmail.com>
2022-11-14 19:40:15 +0000

    controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Container } from 'react-bootstrap';
+
+const navbarStyle={
+  backgroundColor:'lightblue',
+  padding: "10px",
+};
+
 
 const Header = (props) => {
-
   return (
-<Navbar bg="light" variant="light">
+<Navbar style={navbarStyle} variant="light">
+  <Container>
   <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+  </Container>
 </Navbar>)
 }
 
-export default Header ;   
+export default Header ;    
